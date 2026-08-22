eeae232550901cd768e3c9511248bfdc9e7682b4
huntypeteam <info@huntypeteam.hu>
2026-08-22 21:58:14 +0200

    Change request body parsing to JSON format

--- functions/posta-proxy.js
+++ functions/posta-proxy.js
@@ -1,14 +1,22 @@
 export async function onRequestPost(context) {
-  const body = await context.request.text();
+  const body = await context.request.json();
+
+  const searchText = body.city || body.postCode || "";
+
   const response = await fetch(
     "https://net.posta.hu/postoffice/public/postoffice-api/rest/postoffice/search",
     {
       method: "POST",
       headers: { "Content-Type": "application/json" },
-      body
+      body: JSON.stringify({
+        searchField: "város,cím,név",
+        searchText
+      })
     }
   );
+
   const data = await response.text();
+
   return new Response(data, {
     headers: {
       "Content-Type": "application/json",
